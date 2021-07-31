7d0728b650c5aef56a0fea33c4921c51a305fcc5
Yulin WANG <905946576@qq.com>
2021-07-31 07:34:59 +0100

    Test chapter 10
    Successful.

--- rango/views.py
+++ rango/views.py
@@ -18,19 +18,22 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    context_dict['extra'] = 'From the model solution on GitHub'
 
-    request.session.set_test_cookie()
+    visitor_cookie_handler(request)
 
     return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
-    if request.session.test_cookie_worked():
-        print("Test Cookie Worked!")
-        request.session.delete_test_cookie()
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
 
-    return render(request, 'rango/about.html')
+    # if request.session.test_cookie_worked():
+    #     print("Test Cookie Worked!")
+    #     request.session.delete_test_cookie()
+
+    return render(request, 'rango/about.html', context=context_dict)
 
 
 def some_view(request):
@@ -159,3 +162,20 @@ def user_logout(request):
     return redirect(reverse('rango:index'))
 
 
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+    request.session['visits'] = visits
