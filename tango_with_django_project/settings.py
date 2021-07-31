27ec810f1f81d5f775169d85d438715808b9a910
Yulin WANG <905946576@qq.com>
2021-07-31 06:14:37 +0100

    User profile

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -88,6 +88,11 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+
+)
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -95,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
