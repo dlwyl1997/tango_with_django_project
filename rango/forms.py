6c5bb4ac65cc594164beb712fe5b3f2238e31561
Yulin WANG <905946576@qq.com>
2021-07-31 05:38:15 +0100

    Base template

--- rango/forms.py
+++ rango/forms.py
@@ -3,7 +3,7 @@ from rango.models import Page, Category
 
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, help_text="Please enter the category name.")
+    name = forms.CharField(max_length=Category.NAME_MAX_LENGTH, help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -24,9 +24,7 @@ class PageForm(forms.ModelForm):
     def clean(self):
         cleaned_data = self.cleaned_data
         url = cleaned_data.get('url')
-
         if url and not url.startswith('http://'):
             url = f'http://{url}'
             cleaned_data['url'] = url
-
         return cleaned_data
